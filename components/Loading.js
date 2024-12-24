b2406a09ae09da6d8736760ecf7a61877eaa7273
Iny4facelol <150298787+Iny4facelol@users.noreply.github.com>
2024-12-24 10:44:23 +0100

    loading

--- components/Loading.js
+++ components/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Loading() {
   return (
     <div className="flex flex-col flex-1 justify-center items-center">
-      <l-mirage></l-mirage>
+      <l-mirage color="#5850ec"></l-mirage>
     </div>
   );
 }
